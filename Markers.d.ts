686490b8cb4ad4800f53233c075f4ed56f19bfd5
Shmueli Englard <shmueli.yosef@englard.net>
2018-07-18 14:03:34 -0400

    Updated to use namespacing

--- Markers.d.ts
+++ Markers.d.ts
@@ -1,15 +1,18 @@
-interface MarkersGroup
+declare namespace markers
 {
-    groupName: string;
-    icon: string;
-    createInfoWindow: boolean;
-    displayName: string;
-    checked: boolean;
+    interface MarkersGroup
+    {
+        groupName: string;
+        icon: string;
+        createInfoWindow: boolean;
+        displayName: string;
+        checked: boolean;
+    }
+
+    interface Markers
+    {
+        [world: string]: MarkersGroup[];
+    }
 }
 
-interface Markers
-{
-    [world: string]: MarkersGroup[];
-}
-
-declare const markers: Markers | undefined;
+declare const markers: markers.Markers | undefined;
--- markersDB.d.ts
+++ markersDB.d.ts
@@ -1,23 +1,26 @@
-interface Marker
+declare namespace markersDB
 {
-    text: string;
-    x: number;
-    y: number;
-    z: number;
-    hovertext?: string;
-    icon?: string;
+    interface Marker
+    {
+        text: string;
+        x: number;
+        y: number;
+        z: number;
+        hovertext?: string;
+        icon?: string;
+    }
+
+    interface MarkerGroup
+    {
+        raw: Marker[];
+        name: string;
+        created: boolean;
+    }
+
+    interface MarkerDb
+    {
+        [markerGroup: string]: MarkerGroup;
+    }
 }
 
-interface MarkerGroup
-{
-    raw: Marker[];
-    name: string;
-    created: boolean;
-}
-
-interface MarkerDb
-{
-    [markerGroup: string]: MarkerGroup;
-}
-
-declare const markersDB: MarkerDb | undefined;
+declare const markersDB: markersDB.MarkerDb | undefined;
